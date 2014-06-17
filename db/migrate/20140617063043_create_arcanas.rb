b0d668e22f0f4edd5a624ee32e85279875ea40bd
parrot-studio <parrot.studio.dev@gmail.com>
2014-06-17 16:28:40 +0900

    アルカナを表現するモデルを追加

---
+++ db/migrate/20140617063043_create_arcanas.rb
@@ -0,0 +1,17 @@
+class CreateArcanas < ActiveRecord::Migration
+  def change
+    create_table :arcanas do |t|
+      t.string  :name,      null: false, limit: 100
+      t.string  :title,                  limit: 200
+      t.integer :rarity,    null: false, limit: 3
+      t.string  :job_type,  null: false, limit: 10
+      t.integer :job_index, null: false
+      t.timestamps
+    end
+    add_index :arcanas, :name
+    add_index :arcanas, :rarity
+    add_index :arcanas, :job_type
+    add_index :arcanas, [:job_type, :job_index]
+    add_index :arcanas, [:job_type, :rarity]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140617063043) do
+
+  create_table "arcanas", force: true do |t|
+    t.string   "name",       limit: 100, null: false
+    t.string   "title",      limit: 200
+    t.integer  "rarity",     limit: 3,   null: false
+    t.string   "job_type",   limit: 10,  null: false
+    t.integer  "job_index",              null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "arcanas", ["job_type", "job_index"], name: "index_arcanas_on_job_type_and_job_index", using: :btree
+  add_index "arcanas", ["job_type", "rarity"], name: "index_arcanas_on_job_type_and_rarity", using: :btree
+  add_index "arcanas", ["job_type"], name: "index_arcanas_on_job_type", using: :btree
+  add_index "arcanas", ["name"], name: "index_arcanas_on_name", using: :btree
+  add_index "arcanas", ["rarity"], name: "index_arcanas_on_rarity", using: :btree
 
 end
