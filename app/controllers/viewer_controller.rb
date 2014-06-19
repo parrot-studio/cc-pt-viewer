cd9b1980fe7ff72e8b96b25ba13ead72db0a73ca
parrot-studio <parrot.studio.dev@gmail.com>
2014-06-19 14:47:08 +0900

    codeをparseして初期値として埋め込む仕組みを追加

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -1,6 +1,7 @@
 class ViewerController < ApplicationController
 
   def index
+    @arcanas = {}
   end
 
   def datas
@@ -14,7 +15,30 @@ class ViewerController < ApplicationController
   end
 
   def pt
+    code = params[:code]
+    (redirect_to root_path; return) if code.blank?
+    @arcanas = parse_pt_code(code)
+    (redirect_to root_path; return) unless @arcanas
     render :index
+  end
+
+  private
+
+  def parse_pt_code(code)
+    part = "([FKPMA]\\d+|N)"
+    parser = /\A(V\d+)#{part * 6}\z/
+    m = code.upcase.match(parser)
+    return unless m
+
+    selector = lambda {|c| c == 'N' ? nil : c}
+    {
+      mem1: selector.call(m[2]),
+      mem2: selector.call(m[3]),
+      mem3: selector.call(m[4]),
+      mem4: selector.call(m[5]),
+      sub1: selector.call(m[6]),
+      sub2: selector.call(m[7]),
+    }
   end
 
 end
