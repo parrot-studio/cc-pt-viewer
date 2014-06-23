eb12644ae28e1830ad3686992331ccfdf79df197
parrot-studio <parrot.studio.dev@gmail.com>
2014-06-23 15:02:10 +0900

    アルカナのバリデーションを追加

--- app/models/arcana.rb
+++ app/models/arcana.rb
@@ -1,2 +1,25 @@
 class Arcana < ActiveRecord::Base
+  default_scope { order('job_type, rarity DESC, job_index DESC') }
+
+  JOB_TYPES = %w|F K P A M|.freeze
+  RARITYS = (1..5).freeze
+
+  validates :name,
+    presence: true,
+    length: {maximum: 100}
+  validates :title,
+    length: {maximum: 200}
+  validates :rarity,
+    presence: true,
+    inclusion: {in: RARITYS}
+  validates :job_type,
+    presence: true,
+    inclusion: {in: JOB_TYPES}
+  validates :job_index,
+    presence: true,
+    numericality: {only_integer: true}
+  validates :job_index,
+    presence: true,
+    length: {maximum: 20}
+
 end
