e79cfd7b71e8e7f075b72fefc30acde9524b53a2
parrot-studio <parrot.studio.dev@gmail.com>
2014-07-10 16:58:03 +0900

    ドラッグアンドドロップをタブレットに対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery-ui/draggable
 //= require jquery-ui/droppable
+//= require jquery.ui.touch-punch
 // require turbolinks
 //= require bootstrap
 //= require hammer
