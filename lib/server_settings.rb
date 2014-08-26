59f245f75b1a38494e0f885ef7e250f9272267b2
parrot-studio <parrot.studio.dev@gmail.com>
2014-08-26 11:37:09 +0900

    data_versionをimport時点で書き換える仕組みに変更

--- lib/server_settings.rb
+++ lib/server_settings.rb
@@ -27,10 +27,33 @@ class ServerSettings < Settingslogic
     split_types(self.additions).map(&:to_s)
   end
 
+  def data_version
+    unless @data_version
+      update_data_version! unless File.exist?(version_file)
+      @data_version = File.read(version_file).chomp
+    end
+    @data_version
+  end
+
+  def update_data_version!(ver = nil)
+    ver ||= format_time(DateTime.now)
+    File.open(version_file, 'w'){|f| f.puts(ver) }
+    @data_version = ver
+  end
+
   private
 
   def split_types(str)
     str.split(' ').reject(&:blank?).uniq.compact
+  end
+
+  def version_file
+    Rails.root.join('tmp', 'data_version')
+  end
+
+  def format_time(t)
+    format("%04d%02d%02d%02d%02d%02d",
+      t.year, t.month, t.day, t.hour, t.min, t.sec)
   end
 
 end
