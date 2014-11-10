02303618b8a40474fcee2d974093d1a0c94edbd4
parrot-studio <parrot.studio.dev@gmail.com>
2014-11-10 14:45:16 +0900

    広告表示を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
     File.read(path)
   end
 
+  def show_ads?
+    action_name == 'index' ? false : true
+  end
+
+  def adsense
+    path = Rails.root.join('tmp', 'adsense.txt')
+    return '' unless File.exist?(path)
+    File.read(path)
+  end
+
+  def amazon
+    path = Rails.root.join('tmp', 'amazon.txt')
+    return '' unless File.exist?(path)
+    File.read(path)
+  end
+
 end
