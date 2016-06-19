eb66feb5acb69dd83e9796308b090eea4c31261f
parrot-studio <parrot.studio.dev@gmail.com>
2016-06-19 10:01:56 +0900

    名前からの検索をボタンで実行するように修正

--- app/assets/javascripts/components/concerns/name_search_form.es6.jsx
+++ app/assets/javascripts/components/concerns/name_search_form.es6.jsx
@@ -10,15 +10,8 @@ class NameSearchForm extends React.Component {
     this.nameStream = new Bacon.Bus()
 
     let arcanaNameStream = this.nameStream
-      .delay(400).debounce(800)
-      .skipDuplicates()
-      .map((n) => {
-        if (n.length > 1){
-          return {name: n}
-        } else if (n.length < 1) {
-          return {}
-        }
-      })
+      .filter((n) => n.length > 1)
+      .map((n) => { return {name: n} })
 
     this.props.queryStream.plug(arcanaNameStream)
 
@@ -28,9 +21,11 @@ class NameSearchForm extends React.Component {
   }
 
   handleChange(e) {
-    this.setState({name: e.target.value}, () => {
-      this.nameStream.push(this.state.name)
-    })
+    this.setState({name: e.target.value})
+  }
+
+  handleClick(e) {
+    this.nameStream.push(this.state.name)
   }
 
   render() {
@@ -39,10 +34,14 @@ class NameSearchForm extends React.Component {
         <form className="form-horizontal">
           <div className="form-group">
             <label className="control-label col-sm-3 col-md-3" htmlFor="arcana-name">名前から検索</label>
-            <div className="col-sm-6 col-md-6">
+            <div className="col-sm-5 col-md-5">
               <input type="text" className="form-control" id="arcana-name"
                 value={this.state.name} placeholder="名前を入力（2文字以上）"
                 onChange={this.handleChange.bind(this)}/>
+            </div>
+            <div>
+              <button type="button" className="btn btn-primary"
+                onClick={this.handleClick.bind(this)}>検索</button>
             </div>
           </div>
         </form>
